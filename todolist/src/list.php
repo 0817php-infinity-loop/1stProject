ff42195b10156f861a6866edbb41d8e4cea976b6
yacci5513 <ckstjddh1@naver.com>
2023-10-10 14:57:00 +0900

    231010 공통부분 추가

---
+++ todolist/src/list.php
@@ -0,0 +1,42 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>메인 페이지</title>
+	<link rel="stylesheet" href="/todolist/src/css/common.css">
+</head>
+<body>
+	<div class="top_container">
+
+	</div>
+	<div class="main_container">
+		<div class="main_container_box">
+			<div class="left_box">
+				<div class="box_layout">box_layout</div>
+			</div>
+				
+			<div class="right_box">
+				<div class="box_layout">box_layout</div>
+			</div>
+
+			<div class="side_box">
+				<div class="side_category bgc_cate1">
+					<a class= "side_text" href="#">--</a>
+				</div>
+				<div class="side_category bgc_cate2">
+					<a class= "side_text" href="#">--</a>
+				</div>
+				<div class="side_category bgc_cate3">
+					<a class= "side_text" href="#">--</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
