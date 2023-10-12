08ad774ed9b4705fec2e54be560120bb2ad5fdce
muhwa91 <muhwa91@gmail.com>
2023-10-12 23:35:09 +0900

    231012 insert 수정완료

--- todolist/src/03_insert.php
+++ todolist/src/03_insert.php
@@ -1,6 +1,6 @@
 <?php
 define("ROOT", $_SERVER["DOCUMENT_ROOT"]."/todolist/src/");//웹서버root
-define("ERROR_MSG_PARAM", "%s필수 입력 사항입니다.");// 파라미터 에러 메세지
+define("ERROR_MSG_PARAM", "%s 필수 입력 사항입니다.");// 파라미터 에러 메세지
 require_once(ROOT."lib/lib_db.php");// DB관련 라이브러리
 
 // post로 request가 있을 때 처리
@@ -9,7 +9,7 @@ $http_method = $_SERVER["REQUEST_METHOD"]; // Method 확인
 $arr_err_msg = []; // 에러 메세지 저장용
 $title = "";
 $content = "";
-$emotion = "";
+$em_id = "";
 
 if($http_method === "POST") {
 	try {
@@ -17,19 +17,22 @@ if($http_method === "POST") {
 		$arr_post = $_POST;		
 
 		$title = isset($_POST["title"]) ? trim($_POST["title"]) : ""; //title 셋팅
-		$content = isset($_POST["content"]) ? trim($_POST["content"]) : "";
-		$emotion = isset($_POST["emotion"]) ? trim($_POST["emotion"]) : "";
-		var_dump($_POST);
+		$content = isset($_POST["content"]) ? trim($_POST["content"]) : ""; //content 셋팅
+		$em_id = isset($_POST["em_id"]) ? trim($_POST["em_id"]) : ""; //em_id 셋팅
+		
 		if($title === "") {
 			$arr_err_msg[] = sprintf(ERROR_MSG_PARAM, "[제목]");
 		}
 		if($content === "") {
 			$arr_err_msg[] = sprintf(ERROR_MSG_PARAM, "[내용]");
 		}
+		if($em_id === "") {
+			$arr_err_msg[] = sprintf(ERROR_MSG_PARAM, "[감정]");
+		}
 		if(count($arr_err_msg) === 0) {
 			
 		// DB 접속
-		if(!todolist_db_conn($conn)) {
+		if(!db_conn($conn)) {
 			// DB Instance 에러
 			throw new Exception("DB Error : PDO Instance");		
 		}
@@ -39,29 +42,30 @@ if($http_method === "POST") {
 		$arr_param = [
 			"title" => $_POST["title"]
 			,"content" => $_POST["content"]
-			,"imotion" => $_POST["emotion"]
+			,"em_id" => $_POST["em_id"]
 		];
 
 		// insert
-		if(!todolist_db_insert_boards($conn, $arr_post)) {
+		if(!db_insert_boards($conn, $arr_post)) {
 			// DB Instance 에러
 			throw new Exception("DB Error : Insert Boards");		
 		}
 		$conn->commit(); // 모든 처리 완료 시 커밋		
 
 		// 리스트 페이지로 이동
-		header("Location: list.php");
+		header("Location: 01_list.php");
 		exit;
 		}
 	} catch(Exception $e) {
 		if($conn !== null){
 			$conn->rollBack();
-		}	
-		// echo $e->getMessage(); // 예외발생 메세지 출력 //v002 del
+		}
+		// var_dump($_POST);	
+		echo $e->getMessage(); // 예외발생 메세지 출력 //v002 del
 		// header("Location: error_test.php/?err_msg={$e->getMessage()}");	//v002 add
 		exit;
 	} finally {
-		todolist_db_destroy_conn($conn); // DB파기
+		db_destroy_conn($conn); // DB파기
 	}	
 }
 ?>
@@ -77,9 +81,8 @@ if($http_method === "POST") {
 <body>
 	<div class="top_container">
 
-	</div>
-
-	<form action="" method="post">
+	</div>	
+	<form action="/todolist/src/03_insert.php" method="post">
         <div class="main_container">
             <div class="main_container_box">
                 <div class="left_box">
@@ -88,19 +91,20 @@ if($http_method === "POST") {
                             <div class="emotion_ma1">
                                 <div class="emotion_lay1">
                                     <label class="emotion_each" for="emotion1">
-                                    <input type="radio" name="emotion" id="emotion1" value="1">
+                                    <input type="radio" name="em_id" id="emotion1" value="1">
                                     <img class="emo" src="/todolist/doc/img/emotion_1.png">
+                                    <img class="emo_1" src="/todolist/doc/img/emotion_op_1.png">
                                     </label>
                                 </div>
                                 <div class="emotion_lay1">
                                     <label class="emotion_each" for="emotion2">
-                                    <input style="zoom:2.0;" type="radio" name="emotion" id="emotion2" value="2">
+                                    <input type="radio" name="em_id" id="emotion2" value="2">
                                     <img class="emo" src="/todolist/doc/img/emotion_2.png">
                                     </label>
                                 </div>
                                 <div class="emotion_lay1">
                                     <label class="emotion_each" for="emotion3">
-                                    <input type="radio" name="emotion" id="emotion3" value="3">
+                                    <input type="radio" name="em_id" id="emotion3" value="3">
                                     <img class="emo" src="/todolist/doc/img/emotion_3.png">
                                     </label>
                                 </div>
@@ -108,19 +112,19 @@ if($http_method === "POST") {
                             <div class="emotion_ma2">
                                 <div class="emotion_lay2">
                                     <label class="emotion_each" for="emotion4">
-                                    <input type="radio" name="emotion" id="emotion4" value="4">
+                                    <input type="radio" name="em_id" id="emotion4" value="4">
                                     <img class="emo" src="/todolist/doc/img/emotion_4.png">
                                     </label>
                                 </div>
                                 <div class="emotion_lay2">
                                     <label class="emotion_each" for="emotion5">
-                                    <input type="radio" name="emotion" id="emotion5" value="5">
+                                    <input type="radio" name="em_id" id="emotion5" value="5">
                                     <img class="emo" src="/todolist/doc/img/emotion_5.png">
                                     </label>
                                 </div>
                                 <div class="emotion_lay2">
                                     <label class="emotion_each" for="emotion6">
-                                    <input type="radio" name="emotion" id="emotion6" value="6">
+                                    <input type="radio" name="em_id" id="emotion6" value="6">
                                     <img class="emo" src="/todolist/doc/img/emotion_6.png">
                                     </label>
                                 </div>
@@ -128,14 +132,14 @@ if($http_method === "POST") {
                             <div class="emotion_ma3">
                                 <div class="emotion_lay3">
                                     <label class="emotion_each" for="emotion7">
-                                    <input type="radio" name="emotion" id="emotion7" value="7">
+                                    <input type="radio" name="em_id" id="emotion7" value="7">
                                     <img class="emo" src="/todolist/doc/img/emotion_7.png">
                                     </label>
                                 </div>
                                 <div class="emotion_lay3">
                                     <label class="emotion_each" for="emotion8">
-                                    <input type="radio" name="emotion" id="emotion8" value="8">
-                                    <img class="emo_1" src="/todolist/doc/img/emotion_8.png">
+                                    <input type="radio" name="em_id" id="emotion8" value="8">
+                                    <img class="emo" src="/todolist/doc/img/emotion_8.png">
                                     </label>
                                 </div>
                             </div>
@@ -169,10 +173,10 @@ if($http_method === "POST") {
                 </div>
                 <div class="side_box">
                     <div class="side_category bgc_cate1">
-                        <button class= "side_text button_text" href="/todolist/doc/design/03_insert.php">작성</button>
+                        <button class= "side_text button_text" href="/todolist/src/03_insert.php">작성</button>
                     </div>
                     <div class="side_category bgc_cate3">
-                        <a class= "side_text" href="/todolist/doc/design/01_list.html">취소</a>
+                        <a class= "side_text" href="/todolist/src/01_list.php/?page=1">취소</a>
                     </div>
                 </div>
             </div>
